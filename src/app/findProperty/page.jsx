a5ecc44a10be1d5dc02a2e9ebd59a7965ef2adb3
Daniel David <dannieimeh@gmail.com>
2024-09-20 12:54:12 +0100

    new

--- src/app/findProperty/page.jsx
+++ src/app/findProperty/page.jsx
@@ -1,4 +1,7 @@
+import Nav from '../../components/nav/Nav';
 import Search from '../../components/search/Search';
+
+import { FaSearchLocation } from 'react-icons/fa';
 
 export const metadata = {
 	title: 'Our Services',
@@ -6,10 +9,13 @@ export const metadata = {
 
 export default function page() {
 	return (
-		<section className='w-full h-full bg-primary'>
-			<section className=' w-[90%] pt-[25%] sm:pt-[15%] md:pt-[6%] service mx-auto text-white'>
+		<section className='w-full h-svh md:h-[75svh] pb-[2%] bg-secondary'>
+			<Nav />
+			<section className=' w-[90%] pt-4 service mx-auto text-black'>
 				<div className=''>
-					<h3 className='mb-4'>Property search filter</h3>
+					<h3 className='mb-2 flex items-center gap-2'>
+						<FaSearchLocation /> <span>Property search filter</span>
+					</h3>
 					<Search />
 				</div>
 			</section>
--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -51,14 +51,14 @@ export default function Search() {
 	};
 
 	return (
-		<main className='flex items-center justify-start gap-4 h-full'>
-			<section className='flex flex-col items-start justify-between gap-4 basis-[30%]'>
+		<main className='flex flex-col md:flex-row items-center justify-start gap-4 md:gap-0 h-full'>
+			<section className='flex flex-col items-start justify-between gap-4  md:basis-[30%]'>
 				<input
 					type='text'
 					value={filter}
 					onChange={handleFilterChange}
 					placeholder='Enter street name'
-					className='text-secondary shadow bg-primary outline-none p-2'
+					className='text-black shadow bg-white outline-none p-1 sm:p-2'
 				/>
 
 				<input
@@ -66,41 +66,13 @@ export default function Search() {
 					value={titleFilter}
 					onChange={handleTitleFilterChange}
 					placeholder='Enter title'
-					className='text-secondary shadow bg-primary outline-none p-2'
+					className='text-black shadow bg-white outline-none p-1 sm:p-2'
 				/>
-
-				<div className='flex items-start justify-start flex-wrap gap-2 w-1/2'>
-					<button
-						onClick={() => handlePurposeFilter('residential')}
-						className='flex items-center justify-center bg-secondary text-white text-base cursor-pointer py-2 px-4 '>
-						Residential
-					</button>
-					<button
-						onClick={() => handlePurposeFilter('commercial')}
-						className='flex items-center justify-center bg-secondary text-white text-base cursor-pointer py-2 px-4 '>
-						Commercial
-					</button>
-					<button
-						onClick={() => handleStatusFilter('for sale')}
-						className='flex items-center justify-center bg-secondary text-white text-base cursor-pointer py-2 px-4 '>
-						For Sale
-					</button>
-					<button
-						onClick={() => handleStatusFilter('for lease')}
-						className='flex items-center justify-center bg-secondary text-white text-base cursor-pointer py-2 px-4 '>
-						For Lease
-					</button>
-					<button
-						onClick={() => handleStatusFilter('for letting')}
-						className='flex items-center justify-center bg-secondary text-white text-base cursor-pointer py-2 px-4 '>
-						For Letting
-					</button>
-				</div>
 
 				<select
 					value={roomsFilter}
 					onChange={handleRoomsFilterChange}
-					className='text-white shadow bg-primary outline-none p-2 cursor-pointer'>
+					className='text-white shadow bg-black outline-none p-1 sm:p-2 cursor-pointer'>
 					<option value=''>Select number of rooms</option>
 					<option value='1'>1 Room</option>
 					<option value='2'>2 Rooms</option>
@@ -109,28 +81,64 @@ export default function Search() {
 					<option value='5'>5 Rooms</option>
 					<option value='6'>6+ Rooms</option>
 				</select>
+
+				<div className='flex items-start justify-start flex-wrap gap-2 sm:w-1/2'>
+					<button
+						onClick={() => handlePurposeFilter('residential')}
+						className='flex items-center justify-center bg-black text-white text-base cursor-pointer py-1 sm:py-2 px-2 sm:px-4 '>
+						Residential
+					</button>
+
+					<button
+						onClick={() => handlePurposeFilter('commercial')}
+						className='flex items-center justify-center bg-black text-white text-base cursor-pointer py-1 sm:py-2 px-2 sm:px-4 '>
+						Commercial
+					</button>
+
+					<button
+						onClick={() => handleStatusFilter('for letting')}
+						className='flex items-center justify-center bg-black text-white text-base cursor-pointer py-1 sm:py-2 px-2 sm:px-4 '>
+						For Letting
+					</button>
+
+					<button
+						onClick={() => handleStatusFilter('for lease')}
+						className='flex items-center justify-center bg-black text-white text-base cursor-pointer py-1 sm:py-2 px-2 sm:px-4 '>
+						For Lease
+					</button>
+
+					<button
+						onClick={() => handleStatusFilter('for sale')}
+						className='flex items-center justify-center bg-black text-white text-base cursor-pointer py-1 sm:py-2 px-2 sm:px-4 '>
+						For Sale
+					</button>
+				</div>
 			</section>
 
 			<article className='basis-[70%]'>
-				<ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4  w-[90%]  mx-auto '>
+				<ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4  w-full  mx-auto '>
 					{properties.map((property) => (
 						<li
 							key={property.id}
-							className='p-4  rounded shadow cursor-pointer bg-primaryVariant'
+							className='p-4  rounded shadow cursor-pointer bg-white'
 							onClick={() => handlePropertySelect(property)}>
 							<div className='flex flex-col'>
-								<div className='w-[10rem] h-[10rem] basis-2/4 '>
+								<div className='w-full h-[10rem] sm:h-[15rem]  mb-4 '>
 									<Image
 										src={property.image}
 										alt='property image'
 										width={0}
 										height={0}
 										sizes='100%'
-										style={{ width: '100%', height: '100%' }}
+										style={{
+											width: '100%',
+											height: '100%',
+											objectFit: 'cover',
+										}}
 									/>
 								</div>
 
-								<p className='text-white text-base'>
+								<p className='text-black text-base'>
 									{property.name} - {property.address}
 								</p>
 							</div>
